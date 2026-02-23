5e4a1912d7c5118d34e398894f333835cc63e079
Francesco Silvetti <francescosilvetti8@gmail.com>
2026-02-22 21:30:17 -0300

    Fix pyenv included in common.zsh

--- zsh/.zsh/common.zsh
+++ zsh/.zsh/common.zsh
@@ -13,9 +13,5 @@ bindkey '^H' backward-kill-word
 alias gs="git status"
 alias tm="tmux"
 
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-
 eval "$(zoxide init zsh)"
 eval "$(starship init zsh)"
